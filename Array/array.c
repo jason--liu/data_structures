766680f1f8b4660fa411ca6a3331814b5ceb95fe
qianyi.liu <liuqianyi125@gmail.com>
2023-04-19 07:45:30 +0800

    static create array
    Signed-off-by: qianyi.liu <liuqianyi125@gmail.com>

--- Array/array.c
+++ Array/array.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 struct Array {
-    int* A;
+    int A[20];
     int size;
     int length;
 };
@@ -10,7 +10,7 @@ struct Array {
 void Display(struct Array arr)
 {
     int i;
-    printf("\nElements are\n");
+    printf("Elements are:\n");
     for (i = 0; i < arr.length; i++)
         printf("%d ", arr.A[i]);
     printf("\n");
@@ -18,22 +18,7 @@ void Display(struct Array arr)
 
 int main()
 {
-    struct Array arr;
-    int n, i;
-
-    printf("Enter size of an array\n");
-    scanf("%d", &arr.size);
-
-    arr.A = (int*)malloc(arr.size * sizeof(int));
-    arr.length = 0;
-
-    printf("Enter number of numbers\n");
-    scanf("%d", &n);
-
-    printf("Enter all Elements\n");
-    for (i = 0; i < n; i++)
-        scanf("%d", &arr.A[i]);
-    arr.length = n;
+    struct Array arr = {{2, 3, 4, 5, 6}, 20, 5};
 
     Display(arr);
 
