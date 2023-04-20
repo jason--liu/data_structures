44db6c3ce78cc330b8e9ba75d5f87ccdfd841ca5
qianyi.liu <liuqianyi125@gmail.com>
2023-04-20 08:33:16 +0800

    Array: add binary search method.
    Signed-off-by: qianyi.liu <liuqianyi125@gmail.com>

--- Array/array.c
+++ Array/array.c
@@ -75,6 +75,42 @@ int LinearSearch(struct Array* arr, int key)
     return -1;
 }
 
+int BinarySearch(struct Array arr, int key)
+{
+    int l, mid, h;
+
+    l = 0;
+    h = arr.length - 1;
+
+    while (l <= h) {
+        mid = (l + h) / 2;
+        if (key == arr.A[mid])
+            return mid;
+        else if (key < arr.A[mid])
+            h = mid - 1;
+        else
+            l = mid + 1;
+    }
+
+    return -1;
+}
+
+int RBinarySearch(int a[], int l, int h, int key)
+{
+    int mid;
+
+    if (l <= h) {
+        mid = (l + h) / 2;
+        if (key == a[mid])
+            return mid;
+        else if (key < a[mid])
+            return RBinarySearch(a, l, mid - 1, key);
+        else
+            return RBinarySearch(a, mid + 1, h, key);
+    }
+    return -1;
+}
+
 int main()
 {
     struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
@@ -82,7 +118,8 @@ int main()
     /* Append(&arr, 7); */
     /* Insert(&arr, 4, 10); */
     /* printf("%d\n", Delete(&arr, 4)); */
-    printf("%d\n", LinearSearch(&arr, 4));
+    /* printf("%d\n", BinarySearch(arr, 3)); */
+    printf("%d\n", RBinarySearch(arr.A, 0, arr.length, 6));
     Display(arr);
 
     return 0;
